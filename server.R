48d69cd71c18f14e76a806e6006fa5554d7a48eb
midraed <guillermo.olmedo@gmail.com>
2017-01-30 09:21:27 -0300

    First version

---
+++ server.R
@@ -0,0 +1,195 @@
+library(shiny)
+library(DBI)
+library(water)
+
+
+
+shinyServer(function(input, output, session) {
+  
+  ######### Downloading data from the DB ##################################
+  
+  #### Datos METEO
+  
+  datos.WS <- reactive({
+    connWS = dbConnect(MySQL(), user='shiny', password='561234', dbname='METEO1',
+                        host='172.21.116.72')
+    on.exit(dbDisconnect(connWS), add = TRUE)
+    datos <- dbGetQuery(connWS, paste0("SELECT * FROM Current WHERE FechaHora BETWEEN '", input$start1, "' AND '", paste(input$stop1, "23:59:59"), "'" ))
+    # datos$FechaHora <- as.POSIXct(strptime(datos$FechaHora, format= "%Y-%m-%d %H:%M:%S", tz="ART"))
+    #dbDisconnect(datosWS)
+    if(length(datos)>1){
+      datos <- read.WSdata(WSdata = datos, 
+                           datetime.format =  "%Y-%m-%d %H:%M:%S", columns = c("datetime", "temp", 
+                                                                               "RH", NA, "rain", "radiation", "wind"), lat=-33.00513, 
+                           long= -68.86469, elev=927, height= 2)}
+    else datos <-  NA
+    
+  })
+  
+  ### Parcelas y riego
+  
+  datos.goteo <- reactive({
+    connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                          host='172.21.116.72')
+    on.exit(dbDisconnect(connRiego), add = TRUE)
+    datos <- dbGetQuery(connRiego, "SELECT * FROM GOTEO")
+    })
+  
+  datos.superf <- reactive({
+    connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                          host='172.21.116.72')
+    on.exit(dbDisconnect(connRiego), add = TRUE)
+    datos <- dbGetQuery(connRiego, "SELECT * FROM SUPERFICIAL")
+  })
+  
+  datos.Kc <- reactive({
+    connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                          host='172.21.116.72')
+    on.exit(dbDisconnect(connRiego), add = TRUE)
+    datos <- dbGetQuery(connRiego, "SELECT * FROM KC")
+  })
+  
+  datos.riego <- reactive({
+    connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                          host='172.21.116.72')
+    on.exit(dbDisconnect(connRiego), add = TRUE)
+    datos <- dbGetQuery(connRiego, "SELECT * FROM RIEGOS")
+  })
+  
+  ################### RESULTS ###################################
+  
+  ### Sync dates
+  
+  observe({
+    start <- input$start1
+    stop <- input$stop1
+    updateDateInput(session, "start2",
+                    value = start)
+    updateDateInput(session, "stop2",
+                    value = stop)
+    updateDateInput(session, "start3",
+                    value = start)
+    updateDateInput(session, "stop3",
+                    value = stop)
+  })
+  
+  observe({
+    start <- input$start2
+    stop <- input$stop2
+    updateDateInput(session, "start1",
+                    value = start)
+    updateDateInput(session, "stop1",
+                    value = stop)
+    updateDateInput(session, "start3",
+                    value = start)
+    updateDateInput(session, "stop3",
+                    value = stop)
+  })
+  
+  observe({
+    start <- input$start3
+    stop <- input$stop3
+    updateDateInput(session, "start2",
+                    value = start)
+    updateDateInput(session, "stop2",
+                    value = stop)
+    updateDateInput(session, "start1",
+                    value = start)
+    updateDateInput(session, "stop1",
+                    value = stop)
+  })
+  
+
+  
+  
+  ######### Agromet
+  
+  output$WSplot <- renderPlot({
+    plot(datos.WS(), hourly=!input$todos.los.datos)
+  })
+  
+  output$downloadWS <- downloadHandler(
+    filename = function() {
+      paste("INTA-Meteo-", input$start1, "-", input$stop1, ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(datos.WS()$hourly, file)
+    }
+  )
+  
+  
+ ########## ET
+  ETo_values <- reactive({
+    datos <- datos.WS()
+    dias <- unique(datos$hourly$date)
+    ETo_values <- vector()
+    for(i in 1:nrow(datos$hourly)){
+        date <- as.POSIXlt(datos$hourly[i,1], format="%Y-%m-%d %H:%M:%S")
+        ETo_values <- c(ETo_values, hourlyET(datos$hourly[i,], lat=-33.00513, 
+                                         long = -68.86469, elev=927, ET="ETr", 
+                                         height = 2))
+      }
+    ETo_dailys <- vector()
+    for(j in 1:length(dias)){
+      ETo_dailys <- c(ETo_dailys, sum(ETo_values[datos$hourly$date==dias[j]]))
+    }
+    ETo_dailys <- round(ETo_dailys,2)
+    ETo_values <- list()
+    ETo_values$dias <- dias
+    ETo_values$ETo <- ETo_dailys
+    return(ETo_values)
+    })
+  
+  output$ETo_acum <- renderText({paste("ETo acumulada", sum(ETo_values()$ETo), "mm.")})
+  
+  
+  output$EToplot <- renderPlot({
+    barplot(ETo_values()$ETo, names.arg=ETo_values()$dias, main="ETo", col="blue")
+  })
+  
+  output$ETcplot <- renderPlot({
+    if(input$Parcela1 != ""){
+      parcelas <- datos.goteo()
+      IDKc <- datos.Kc()$ID[datos.Kc()$Nombre==input$Kc]
+      meses <- as.POSIXlt(ETo_values()$dias)$mon + 3
+      Kc <- as.numeric(datos.Kc()[IDKc,meses])
+      bp <- barplot(ETo_values()$ETo * Kc, names.arg=ETo_values()$dias, main="ETc", col="dark green")
+      lines(bp[,1], Kc, type = "o", ylim=c(0,1.2))
+      }
+  })
+  
+ ######### Riego
+  Lam <- 238
+  ppT <- 54
+  ppE <- 46
+  
+  lamina_aplicada <- reactive({
+    connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                          host='172.21.116.72')
+    on.exit(dbDisconnect(connRiego), add = TRUE)
+    datos <- dbGetQuery(connRiego, paste0("SELECT * FROM RIEGOS WHERE Fecha BETWEEN '", input$start3, "' AND '", input$stop3, 
+                                          "' AND N_PARCELA = 14"))
+    # Lámina aplicada=CAUDAL_GOT*Horas de Riego*((100/DIST_HIL)*(100/DIST_GOT))/1000/10
+    inParcela <- paste(datos.goteo()$N_PARCELA, datos.goteo()$PARCELA)
+    datos.goteo <- datos.goteo()[inParcela == input$Parcela2,]
+    lamina_aplicada <- list()
+    lamina_aplicada$riegos <- datos.goteo$CAUDAL_GOT*datos$`Horas de Riego`*((100/datos.goteo$DIST_HILERAS)*(100/datos.goteo$DIST_GOT))/1000/10
+    lamina_aplicada$fechas <- datos$Fecha
+    return(lamina_aplicada)
+  })
+  
+  
+  output$Lam_acum <- renderText({paste("Lámina de riego acumulada", round(sum(lamina_aplicada()$riegos),0), "mm.")})
+  output$ppT_acum <- renderText({paste("Precipitación total acumulada", sum(datos.WS()$hourly$rain), "mm.")})
+  output$ppE_acum <- renderText({paste("Precipitación efectiva acumulada", round(sum(datos.WS()$hourly$rain)*0.6,0), "mm.")})
+  
+  output$riegoPlot <- renderPlot({
+    if(input$Parcela2 != ""){
+      df1 <- data.frame(fechas=as.character(seq.Date(input$start3, input$stop3, by=1)))
+      df2 <- data.frame(fechas=lamina_aplicada()$fechas, riegos=lamina_aplicada()$riegos)
+      result <- base::merge(df1,df2, all.x=T)
+      barplot(result$riegos, names.arg=result$fechas, col="light blue")
+    }
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,87 @@
+library(shiny)
+library(water)
+library(RMySQL)
+
+connRiego = dbConnect(MySQL(), user='shiny', password='561234', dbname='RIEGO',
+                        host='172.21.116.72')
+datos.goteo <- dbGetQuery(connRiego, "SELECT * FROM GOTEO")
+datos.superficial <- dbGetQuery(connRiego, "SELECT * FROM SUPERFICIAL")
+datos.kc <- dbGetQuery(connRiego, "SELECT * FROM KC")
+dbDisconnect(connRiego)
+
+listaParcelas <- list('Goteo' = paste(datos.goteo$N_PARCELA, datos.goteo$PARCELA), "Superficie" = paste(datos.superficial$N_PARCELA, datos.superficial$PARCELA))
+
+listaKc <- datos.kc$Nombre
+
+
+navbarPage("Riego EEA Mendoza!",
+           tabPanel("Agrometeorología",
+                    sidebarLayout(
+                      sidebarPanel(
+                        dateInput("start1", "Desde:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        dateInput("stop1", "Hasta:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        checkboxInput("todos.los.datos", "Todos los datos", FALSE),
+                        downloadButton("downloadWS", "Descargar")
+                      ),
+                      
+                      # Show a plot of the generated distribution
+                      mainPanel(
+                        #tableOutput("table")
+                        plotOutput("WSplot")
+                      )
+                    )  
+           ),
+           tabPanel("Evapotranspiración",
+                    sidebarLayout(
+                      sidebarPanel(
+                        dateInput("start2", "Desde:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        dateInput("stop2", "Hasta:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        selectInput("Parcela1", "Parcela:", c("", listaParcelas), selected = NULL, multiple = FALSE,
+                                    selectize = TRUE, width = NULL, size = NULL),
+                        selectInput("Kc", "Kc:", listaKc, selected = NULL, multiple = FALSE,
+                                    selectize = TRUE, width = NULL, size = NULL),
+                        numericInput("Kc_medio", "Kc medio:", value = 0,  min = 0, max = 2)
+                      ),
+                      
+                      # Show a plot of the generated distribution
+                      mainPanel(
+                       textOutput("ETo_acum"),
+                       plotOutput("EToplot"),
+                       plotOutput("ETcplot")
+                      )
+                    )  
+           ),
+           tabPanel("Riego",
+                    sidebarLayout(
+                      sidebarPanel(
+                        dateInput("start3", "Desde:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        dateInput("stop3", "Hasta:", value = NULL, min = NULL, max = NULL,
+                                  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                  language = "es", width = NULL),
+                        selectInput("Parcela2", "Parcela:", listaParcelas, selected = NULL, multiple = FALSE,
+                                    selectize = TRUE, width = NULL, size = NULL)
+                      ),
+                      
+                      # Show a plot of the generated distribution
+                      mainPanel(
+                        textOutput("Lam_acum"),
+                        textOutput("ppT_acum"),
+                        textOutput("ppE_acum"),
+                        plotOutput("riegoPlot")
+                        # #tableOutput("table")
+                       
+                      )
+                    )  
+           )
+           
+) #END
