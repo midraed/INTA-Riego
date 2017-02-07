108dc22955e8c8a0e4f82d1e93a08aa802517875
midraed <guillermo.olmedo@gmail.com>
2017-02-07 12:52:33 -0300

    Kc_values mas legible

--- server.R
+++ server.R
@@ -143,7 +143,7 @@ shinyServer(function(input, output, session) {
     }
     ETo_dailys <- round(ETo_dailys,2)
     ETo_values <- list()
-    ETo_values$dias <- dias
+    ETo_values$dias <- dias  ## <- Esta pasando horas y no dias!
     ETo_values$ETo <- ETo_dailys
     return(ETo_values)
   })
@@ -151,26 +151,25 @@ shinyServer(function(input, output, session) {
   Kc_values <- reactive({
     if(input$Parcela1 != ""){
       Kc_values <- list()
-      IDKc <- datos.Kc()$ID[datos.Kc()$Nombre==input$Kc]
+      IDKc <- datos.Kc()$ID[datos.Kc()$Nombre==input$Kc] ## Solo el ID
       meses <- as.POSIXlt(ETo_values()$dias)$mon
-      Kc_values$Kc <- as.numeric(datos.Kc()[IDKc,meses+3])
-      Kc_values$meses <- meses
-      ### Update sliders
       meses_names <- c("enero", "febrero", "marzo", "abril", "mayo", "junio", 
-                       "julio", "agosto", "setiembre", "noviembre", "diciembre") #Seguro que esta en Constants
+                       "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre") #Seguro que esta en Constants
+      Kc_values <- unique(data.frame(mes=meses_names[meses+1], Kc=as.numeric(datos.Kc()[IDKc,meses+3])))
+      ### Update sliders
       ### TODO: Hace un bucle nabo
-      updateSliderInput(session, "Kc_mes1", label = paste("Kc" , meses_names[unique(meses)[1]+1]), value = unique(Kc_values$Kc)[1])
-      updateSliderInput(session, "Kc_mes2", label = paste("Kc" , meses_names[unique(meses)[2]+1]), value = unique(Kc_values$Kc)[2])
-      updateSliderInput(session, "Kc_mes3", label = paste("Kc" , meses_names[unique(meses)[3]+1]), value = unique(Kc_values$Kc)[3])
-      updateSliderInput(session, "Kc_mes4", label = paste("Kc" , meses_names[unique(meses)[4]+1]), value = unique(Kc_values$Kc)[4])
-      updateSliderInput(session, "Kc_mes5", label = paste("Kc" , meses_names[unique(meses)[5]+1]), value = unique(Kc_values$Kc)[5])
-      updateSliderInput(session, "Kc_mes6", label = paste("Kc" , meses_names[unique(meses)[6]+1]), value = unique(Kc_values$Kc)[6])
-      updateSliderInput(session, "Kc_mes7", label = paste("Kc" , meses_names[unique(meses)[7]+1]), value = unique(Kc_values$Kc)[7])
-      updateSliderInput(session, "Kc_mes8", label = paste("Kc" , meses_names[unique(meses)[8]+1]), value = unique(Kc_values$Kc)[8])
-      updateSliderInput(session, "Kc_mes9", label = paste("Kc" , meses_names[unique(meses)[9]+1]), value = unique(Kc_values$Kc)[9])
-      updateSliderInput(session, "Kc_mes10", label = paste("Kc" , meses_names[unique(meses)[10]+1]), value = unique(Kc_values$Kc)[10])
-      updateSliderInput(session, "Kc_mes11", label = paste("Kc" , meses_names[unique(meses)[11]+1]), value = unique(Kc_values$Kc)[11])
-      updateSliderInput(session, "Kc_mes12", label = paste("Kc" , meses_names[unique(meses)[12]+1]), value = unique(Kc_values$Kc)[12])
+      updateSliderInput(session, "Kc_mes1", label = paste("Kc" , Kc_values$mes[1]), value = Kc_values$Kc[1])
+      updateSliderInput(session, "Kc_mes2", label = paste("Kc" , Kc_values$mes[2]), value = Kc_values$Kc[2])
+      updateSliderInput(session, "Kc_mes3", label = paste("Kc" , Kc_values$mes[3]), value = Kc_values$Kc[3])
+      updateSliderInput(session, "Kc_mes4", label = paste("Kc" , Kc_values$mes[4]), value = Kc_values$Kc[4])
+      updateSliderInput(session, "Kc_mes5", label = paste("Kc" , Kc_values$mes[5]), value = Kc_values$Kc[5])
+      updateSliderInput(session, "Kc_mes6", label = paste("Kc" , Kc_values$mes[6]), value = Kc_values$Kc[6])
+      updateSliderInput(session, "Kc_mes7", label = paste("Kc" , Kc_values$mes[7]), value = Kc_values$Kc[7])
+      updateSliderInput(session, "Kc_mes8", label = paste("Kc" , Kc_values$mes[8]), value = Kc_values$Kc[8])
+      updateSliderInput(session, "Kc_mes9", label = paste("Kc" , Kc_values$mes[9]), value = Kc_values$Kc[9])
+      updateSliderInput(session, "Kc_mes10", label = paste("Kc" , Kc_values$mes[10]), value = Kc_values$Kc[10])
+      updateSliderInput(session, "Kc_mes11", label = paste("Kc" , Kc_values$mes[11]), value = Kc_values$Kc[11])
+      updateSliderInput(session, "Kc_mes12", label = paste("Kc" , Kc_values$mes[12]), value = Kc_values$Kc[12])
       ### End update sliders
       return(Kc_values)
     }
@@ -182,7 +181,7 @@ shinyServer(function(input, output, session) {
   
   output$ETc_acum <- renderText({
     if(input$Parcela1 != ""){
-      Kc_slider <- as.numeric(as.factor(Kc_values()$meses))
+      Kc_slider <- as.numeric(as.factor(Kc_values()$meses))  ## <- Aca esta cambiado KC_values y no funca
       Kc_value <- as.numeric(as.factor(Kc_values()$meses))
       Kc_value[Kc_slider==1] <- input$Kc_mes1
       Kc_value[Kc_slider==2] <- input$Kc_mes2
